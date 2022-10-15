e3bc59b108bd7835f0c583e298d75536a9be92f4
qixiao <niudongze123@gmail.com>
2022-10-15 20:40:28 +0800

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -328,7 +328,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.offsetEncoding = "utf-8"
 capabilities.textDocument.foldingRange = {
     dynamicRegistration = false,
